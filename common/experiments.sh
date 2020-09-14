71350a59283e394e18546e04630dcdabd530ca13
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-09-14 19:54:41 +0200

    Reorganize current queries.

--- common/experiments.sh
+++ common/experiments.sh
@@ -43,7 +43,7 @@ function run_one {
 		EOF
 
     (
-        cat "$system_dir/queries/$query."*q | "$system_dir/$platform/run.sh"
+        cat "$system_dir/$platform/queries/$query."*q | "$system_dir/$platform/run.sh"
         echo "Exit code: $?"
     ) 2>&1 | tee "$run_result_dir"/run.log
 }
--- rumble/experiments.sh
+++ rumble/experiments.sh
@@ -7,6 +7,6 @@ SYSTEM="rumble"
 . "$SOURCE_DIR/../common/experiments.sh"
 
 NUM_RECORDS=($((2**0*10000)) $((2**1*10000)) $((2**2*10000)) $((2**3*10000)) $((2**4*10000)) $((2**5*10000)) $((2**6*10000)) $((2**7*10000)) $((2**8*10000)) $((2**9*10000)))
-QUERIES=(count-star edbt18-q00 edbt18-q01 edbt18-q02 filter grouping_large grouping_small sorting)
+QUERIES=(weather-count-star weather-q00 weather-q01 weather-q02 weather-filter weather-grouping_large weather-grouping_small weather-sorting)
 
 run_many "singlecore" NUM_RECORDS QUERIES
