4f0f2def9537d9dc58a4525666a2ed47d950dd39
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-09-15 21:59:03 +0200

    Make enumeration of data set sizes more concise.

--- rumble/experiments.sh
+++ rumble/experiments.sh
@@ -6,7 +6,7 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SYSTEM="rumble"
 . "$SOURCE_DIR/../common/experiments.sh"
 
-NUM_RECORDS=($((2**0))mb $((2**1))mb $((2**2))mb $((2**3))mb $((2**4))mb $((2**5))mb $((2**6))mb $((2**7))mb $((2**8))mb $((2**9))mb)
+NUM_RECORDS=($(for i in {0..9}; do echo $((2**$i))mb; done))
 QUERIES=(weather-count-star weather-q00 weather-q01 weather-q02 weather-filter weather-grouping_large weather-grouping_small weather-sorting)
 
 run_many "singlecore" NUM_RECORDS QUERIES
