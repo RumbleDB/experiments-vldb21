0d01a5296b7c6d242069adc1975372945d828559
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-10-10 11:51:50 +0200

    Make Snowflake script more configurable.

--- snowflake/experiments.sh
+++ snowflake/experiments.sh
@@ -9,6 +9,8 @@ fi
 
 NUM_RUNS=5
 INPUT_SIZES=($(for i in {0..9}; do echo --input_size samples/$((2**$i*20))mb; done))
+CLUSTER_SIZE="LARGE"
+QUERY_FILTER="" # leave empty for all queries, use "-k name" to run queries containing "name"
 
 # Create result dir
 experiments_dir="$SOURCE_DIR/experiments"
@@ -17,5 +19,7 @@ mkdir -p "$result_dir"
 
 # Run
 (
-    "$SOURCE_DIR/experiments.py" -v ${INPUT_SIZES[*]} --count $NUM_RUNS
+    "$SOURCE_DIR/experiments.py" -v \
+        ${INPUT_SIZES[*]} --count $NUM_RUNS \
+        --warehouse_size "$CLUSTER_SIZE" $QUERY_FILTER
 ) 2>&1 | tee "$result_dir/run.log"
